5ba9dedfad927a61398fd23a1a496fe169c6a576
al rakib sorder <46457169+saikat111@users.noreply.github.com>
2020-04-05 15:04:17 +0600

    Add files via upload

--- Array.java
+++ Array.java
@@ -1,38 +1,73 @@
 import  java.util.Scanner;
 public class Array {
-    public static  void main(String args[]){
-        int n;
-        int value;
-        int d ;
-        System.out.println("Enter the array size:");
-        Scanner obj = new Scanner(System.in);
-        n = obj.nextInt();
-        int[] list = new int[n];
-        int[] newArr = new int[list.length - 1];
-        System.out.println("Enter the array element:");
-        for(int i = 0; i<list.length;i++){
-            list[i] = obj.nextInt();
+    public static void main(String args[]){
+        int arraySize, arrayIndexNumberForInsertingTheElement, insertedElement;
+        int positionForInsertingTheElement;
+        Scanner objectOfScannnerClass = new Scanner(System.in);
+        System.out.print("Enter no. of elements you want in array:");
+        arraySize = objectOfScannnerClass.nextInt();
+        int array[] = new int[arraySize+1];
+        System.out.println("Enter all the elements:");
+        for(int i = 0; i < arraySize; i++)
+        {
+            array[i] = objectOfScannnerClass.nextInt();
         }
-        System.out.println("Enter the value you want to search:");
-        value = obj.nextInt();
-        for(int i = 0; i<list.length;i++){
-           if (list[i] == value){
-               System.out.println("value found");
+        System.out.println("Enter the positon where you want to insert element:");
+        System.out.println("For Front type 1, for middle type 2,for end type 3:");
+        positionForInsertingTheElement = objectOfScannnerClass.nextInt();
 
+       if(positionForInsertingTheElement == 3){
+           System.out.print("Enter the element you want to insert:");
+           insertedElement = objectOfScannnerClass.nextInt();
+           arrayIndexNumberForInsertingTheElement = array.length;
+           for(int i = (arraySize-1); i >= (arrayIndexNumberForInsertingTheElement-1); i--)
+           {
+               array[i+1] = array[i];
            }
-        }
-        System.out.println("Enter the element you want to delete:");
-        d = obj.nextInt();
-        // Copying the elements to the new array except the index from the original array.
-        for(int j=0, k=0; j<list.length; j++) {
-            if(j == d)
-                continue;
-            newArr[k++] = list[j];
-        }
-        for(int i = 0; i<list.length-1;i++){
-            System.out.print(newArr[i]);
+           array[arrayIndexNumberForInsertingTheElement-1] = insertedElement;
+           System.out.println("After inserting:");
+           System.out.println("Elements are:");
+           for(int i = 0; i < arraySize; i++)
+           {
 
+               System.out.print(array[i]+",");
+           }
+           System.out.print(array[arraySize]);
+       }
+       else if(positionForInsertingTheElement == 2){
+            System.out.print("Enter the element you want to insert:");
+            insertedElement = objectOfScannnerClass.nextInt();
+            arrayIndexNumberForInsertingTheElement = array.length / 2;
+            for(int i = (arraySize-1); i >= (arrayIndexNumberForInsertingTheElement-1); i--)
+            {
+                array[i+1] = array[i];
             }
-
+            array[arrayIndexNumberForInsertingTheElement-1] = insertedElement;
+           System.out.println("After inserting:");
+           System.out.println("Elements are:");
+            for(int i = 0; i < arraySize; i++)
+            {
+                System.out.print(array[i]+",");
+       }
+            System.out.print(array[arraySize]);
+        }
+       if(positionForInsertingTheElement == 1){
+            System.out.print("Enter the element you want to insert:");
+            insertedElement = objectOfScannnerClass.nextInt();
+            arrayIndexNumberForInsertingTheElement = 1;
+            for(int i = (arraySize-1); i >= (arrayIndexNumberForInsertingTheElement-1); i--)
+            {
+                array[i+1] = array[i];
+            }
+            array[arrayIndexNumberForInsertingTheElement-1] = insertedElement;
+           System.out.println("After inserting:");
+           System.out.println("Elements are:");
+            for(int i = 0; i < arraySize; i++)
+            {
+                System.out.print(array[i]+",");
+            }
+            System.out.print(array[arraySize]);
+       }
     }
 }
+
